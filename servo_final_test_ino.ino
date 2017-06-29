6f25296bdb16514b675e5991e648bb749519b93e
Upasanasingh4114 <singhupasana4114@gmail.com>
2017-06-29 19:03:23 +0530

    Update servo_final_test_ino.ino

--- servo_final_test_ino.ino
+++ servo_final_test_ino.ino
@@ -6,28 +6,41 @@ Servo servo3;
 Servo servo4;
 Servo servo5;
 Servo servo6;
+Servo servo7;
+Servo servo8;
+Servo servo9;
 
-int a1 = 0;
-int a2 = 0;
-int a3 = 0;
-int a4 = 0;
-int a5 = 0;
-int a6 = 0;
+//=====================================================================================================================================================================//
+//angles.........0...0...90...90...0...0
+// first preference is always given to right servo
+//=====================================================================================================================================================================//
+int a1 = 0; //wrist
+int a2 = 0; //wrist
+int a3 = 90; //elbow
+int a4 = 90; //elbow
+int a5 = 0; //shoulder
+int a6 = 0; //base8
 
 void setup(){
   servo1.attach(1);
-  servo1.attach(2);
-  servo1.attach(3);
-  servo1.attach(4);
-  servo1.attach(5);
-  servo1.attach(6);
+  servo2.attach(2);
+  servo3.attach(3);
+  servo4.attach(4);
+  servo5.attach(5);
+  servo6.attach(6);
+  servo7.attach(7);
+  servo8.attach(8);
+  servo9.attach(9);
 }
 
 void loop(){
   servo1.write(a1);
-  servo1.write(a2);
-  servo1.write(a3);
-  servo1.write(a4);
-  servo1.write(a5);
-  servo1.write(a6);
+  servo2.write(a2);
+  servo3.write(a3);
+  servo4.write(a3);
+  servo5.write(a4);
+  servo6.write(a4);
+  servo7.write(a5);
+  servo8.write(a5);
+  servo9.write(a6);
 }
